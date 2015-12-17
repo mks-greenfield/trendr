6620d2db444cbc61605a702c40e6a9ab357a1859
kcvan <kcongvan@gmail.com>
2015-12-17 11:50:04 -0800

    [feat] Add requires to app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var request = require('request');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var twitter = require('twitter');
+var fs = require('fs');
+var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
