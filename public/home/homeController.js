0916bc775bdd3599480f0057ae2090a16ceeb31f
Simon Ding <simonding87@Simons-MacBook-Pro.local>
2015-12-17 20:11:12 -0800

    [feat] Add spinner button when waiting for data, also some refactor

--- public/home/homeController.js
+++ public/home/homeController.js
@@ -3,19 +3,4 @@ angular.module('home', [])
 .controller('homeController', function($scope, $location, $http) {
   $scope.message = "Welcome to the home page";
 
-  $scope.getTrends = function() {
-    $location.url('/chart');
-    $http({
-      method: 'GET',
-      url: '/api'
-    })
-    .then(successCallback, errorCallback);
-  }
-
-  var successCallback = function() {
-    // want to load new data into charts
-  };
-  var errorCallback = function() {
-    // handle error
-  };
 })
