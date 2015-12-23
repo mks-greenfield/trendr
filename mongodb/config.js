0f6d1ae136f70241d005054f2064e5d3b6ae3fab
Simon Ding <simonding87@simons-mbp.attlocal.net>
2015-12-22 16:27:35 -0800

    [cleanup] Remove config.js from .gitignore, config.js is for our db connection

---
+++ mongodb/config.js
@@ -0,0 +1,16 @@
+// require('dotenv').load(); //loads .env vars
+var mongoose = require('mongoose');
+
+local: mongoose.connect('mongodb://localhost/test');
+//mongolab:  mongodb://<dbuser>:<dbpassword>@ds035503.mongolab.com:35503/mks-greenfield
+// mongoose.connect(process.env.MONGOLAB_URI);
+
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, 'connection error:'));
+
+db.once("open", function(callback) {
+  console.log("We've opened a connection");
+});
+
+module.exports = db;
