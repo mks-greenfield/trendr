4a67c40c62210d53c5b49934e9063c210d7b136f
Simon Ding <simonding87@simons-mbp.attlocal.net>
2015-12-22 17:34:01 -0800

    [feat] New api call (trends/place) adds data to mongodb

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var path = require('path');
 var mongoose = require('mongoose');
 
 var Trend = require('./mongodb/models/trend.js');
+var LocationTrend = require('./mongodb/models/locationTrend.js');
 
 /*************************************************************
 Local Dependencies
@@ -49,6 +50,54 @@ app.get('/db', function(req, res) {
     res.send(trends);
   });
 });
+
+
+
+
+
+// TESTING NEW API CALL
+
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
+app.get('/test', function(req, res) {
+  console.log('testing');
+  var params = {id: 2487956}; // San Francisco
+  // var params = {lat: 37.7833, long: 122.4167}
+  client.get('trends/place', params, function(error, tweets, response){
+    if (error) {
+      console.log('ERROR OCCURED', error);
+      throw error;
+    }
+    
+    var apiArr = tweets;
+
+    for (var i = 0; i < apiArr[0].trends.length; i++) {
+      new LocationTrend({
+        location: apiArr[0].locations[0].name,
+        trend_name: apiArr[0].trends[i].name,
+        tweet_volume: apiArr[0].trends[i].tweet_volume
+      })
+      .save(function(err) {
+        if(err) throw err;
+        console.log('trend saved!');
+      })
+    };
+
+    res.status(200);
+    res.send(tweets);
+
+  });
+});
+
+
+
 
 /*************************************************************
 Port
