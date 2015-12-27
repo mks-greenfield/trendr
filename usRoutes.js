b46113ff460765731132dcf680cd356e337ced9d
Polina Soshnin <psoshnin@wellesley.edu>
2015-12-26 19:54:43 -0600

    [feat] add four base routes: /cities, /states, /trends, /country

--- usRoutes.js
+++ usRoutes.js
@@ -1,7 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/test', function(req, res) {
+/*************************************************************
+GET /api/us/cities
+**************************************************************/
+
+router.get('/cities', function(req, res) {
+  res.send('testing this route');
+});
+
+/*************************************************************
+GET /api/us/states
+**************************************************************/
+
+router.get('/states', function(req, res) {
+  res.send('testing this route');
+});
+
+/*************************************************************
+GET /api/us/trends
+**************************************************************/
+
+router.get('/trends', function(req, res) {
+  res.send('testing this route');
+});
+
+/*************************************************************
+GET /api/us/country
+**************************************************************/
+
+router.get('/country', function(req, res) {
   res.send('testing this route');
 });
 
