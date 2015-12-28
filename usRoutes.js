eccd20d8e4316bf696f799bd334dd2cf9b674801
Polina Soshnin <psoshnin@wellesley.edu>
2015-12-27 23:58:29 -0600

    [feat] implement api/us/country/today

--- usRoutes.js
+++ usRoutes.js
@@ -2,6 +2,24 @@ var express = require('express');
 var router = express.Router();
 var db = require('./mongodb/config');
 var USTrend = require('./mongodb/models/USTownTrend');
+var async = require('async');
+var _ = require('underscore');
+
+/*************************************************************
+Add Underscore Mixin to sort by keys
+**************************************************************/
+
+_.mixin({
+  'sortKeysBy': function (obj, comparator) {
+    var keys = _.sortBy(_.keys(obj), function (key) {
+      return comparator ? comparator(obj[key], key) : key;
+    });
+
+    return _.object(keys, _.map(keys, function (key) {
+      return obj[key];
+    }));
+  }
+});
 
 /*************************************************************
 GET /api/us/cities
@@ -117,7 +135,7 @@ router.get('/trends/day', function(req, res) {
               console.log("error", err);
 
               res.status(500);
-              res.status("Internal Server Error. Cannot read from database at this time.")
+              res.send("Internal Server Error. Cannot read from database at this time.")
             } else {
 
               res.status(200);
@@ -143,7 +161,7 @@ router.get('/trends/:trendname/city', function(req, res) {
               console.log("error", err);
 
               res.status(500);
-              res.status("Internal Server Error. Cannot read from database at this time.")
+              res.send("Internal Server Error. Cannot read from database at this time.")
             } else {
               console.log("item", item);
               res.status(200);
@@ -169,7 +187,7 @@ router.get('/trends/:trendname/state', function(req, res) {
               console.log("error", err);
 
               res.status(500);
-              res.status("Internal Server Error. Cannot read from database at this time.")
+              res.send("Internal Server Error. Cannot read from database at this time.")
             } else {
               console.log("item", item);
               res.status(200);
@@ -190,10 +208,63 @@ GET /api/us/country/today
 GET /api/us/country/weekly
 **************************************************************/
 
+//returns all trends today in the U.S. ordered by number of cities having that trend.
 router.get('/country/today', function(req, res) {
 
-  res.status(200);
-  res.send("returns top 10 trends today in the U.S. by number of cities having that trend.");
+  var startDate = new Date()  // Current date
+  startDate.setHours(0)   // Set the hour, minute and second components to 0
+  startDate.setMinutes(0)
+  startDate.setSeconds(0)
+
+  var trend_data = {};
+
+  //find all trends created today
+  USTrend.distinct("trend_name")
+         .where({created_at: {$gt: startDate, $lt: new Date(),}})
+         .exec(function(err, trends) {
+            if (err) {
+              console.log("error", err);
+
+              res.status(500);
+              res.send("Internal Server Error. Cannot read from database at this time.")
+            } else {
+
+              async.each(trends, function(trend, next) {
+
+                //return count of which cities had that trend today
+                USTrend.distinct("location_name")
+                       .where({trend_name : trend, created_at: {$gt: startDate, $lt: new Date(),}})
+                       .count()
+                       .exec(function(err, count) {
+                          if (err) {
+                            console.log("error", err);
+
+                          } else {
+                            //console.log("count", count);
+                            trend_data[trend] = count;
+                            next();
+                          }
+                       });
+
+                  
+              }, function(error){
+                  if(error) {
+                    console.log('A town failed to process');
+                  } else {
+                    //console.log('All towns have been processed successfully');
+                    
+                    //sort by city count in descending order
+                    var result =  _.sortKeysBy(trend_data, function (value, key) {
+                      //changes from ascending to descending sort
+                      return -(value);
+                    });
+
+                    res.status(200);
+                    res.send(result);
+                  }
+              });
+            }
+         });
 });
 
 router.get('/country/weekly', function(req, res) {
