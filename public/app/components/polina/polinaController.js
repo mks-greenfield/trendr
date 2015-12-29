64cfd3feab7f09a5f7e0924e1923f57e61032f73
Polina Soshnin <psoshnin@wellesley.edu>
2015-12-29 15:51:47 -0600

    [feat] Add basic drop down menu displaying available states with search function

--- public/app/components/polina/polinaController.js
+++ public/app/components/polina/polinaController.js
@@ -1,5 +1,38 @@
-angular.module('polina', [])
+var app = angular.module('polina', ['ngSanitize','ui.select'])
 
-.controller('PolinaController', function($scope) {
-  $scope.message = "Polina's div";
+app.controller('PolinaController', function($scope, $http) {
+
+  $scope.state = {};
+  $scope.states = [];
+
+  $scope.getStates = function() {
+
+    $http({
+      method: 'GET',
+      url: '/api/us/states'
+    }).then(function successCallback(response) {
+        response.data.sort();
+        angular.forEach(response.data, function(value) {
+          $scope.states.push({name: value});
+        });
+      }, function errorCallback(response) {
+
+      });
+  };
+
+  $scope.getStates();
+
+
+  // $scope.getUSTopTrends = function() {
+
+  //   $http({
+  //     method: 'GET',
+  //     url: 'api/us/country/today'
+  //   }).then(function successCallback(response) {
+  //       $scope.data = response.data;
+  //     }, function errorCallback(response) {
+
+  //     });
+  // };
+
 });
