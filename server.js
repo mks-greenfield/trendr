7a48d1f1f29184a9c4b4c15cd7bd3ce946392691
Simon Ding <simonding87@simons-mbp.attlocal.net>
2015-12-28 20:34:04 -0800

    [feat] Add d3 display for trends in SF and Chicago that are retrieved from mongolab DB

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var mongoose = require('mongoose');
 
+var db = require('./mongodb/config.js');
 var Trend = require('./mongodb/models/trend.js');
 var LocationTrend = require('./mongodb/models/locationTrend.js');
 
@@ -22,11 +23,13 @@ app.use(morgan('combined'));
 //load client side assets
 app.use(express.static(path.join(__dirname, 'public')));
 //client side package manager
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 // Parse
 app.use(bodyParser.json());
 // Parse forms (signup/login)
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 /*************************************************************
 ROUTES FOR /api/us/*
@@ -59,7 +62,16 @@ app.get('/db', function(req, res) {
 
 
 
-
+app.get('/locationTrends', function(req, res) {
+  LocationTrend.find(function(err, trends) {
+    if (err) {
+      throw err;
+    }
+    console.log('locationTrends', trends);
+    res.status(200);
+    res.send(trends);
+  });
+});
 
 // TESTING NEW API CALL
 
@@ -79,7 +91,7 @@ app.get('/test', function(req, res) {
   // Be careful not to call this same location twice or it will duplicate data in the DB.
   // Maybe need to figure out how to prevent this.
 
-   var params = {id: 2487956}; // San Francisco
+  // var params = {id: 2487956}; // San Francisco
   // var params = {id: 2487889}; // San Diego
   // var params = {id: 2379574}; // Chicago
   // var params = {id: 2450022}; // Miami
@@ -94,30 +106,65 @@ app.get('/test', function(req, res) {
   // var params = {id: 2471390}; // Phoenix
   // var params = {id: }; // 
 
-  client.get('trends/place', params, function(error, tweets, response){
-    if (error) {
-      console.log('ERROR OCCURED', error);
-      throw error;
-    }
-    
-    var apiArr = tweets;
+  paramArray = [{
+        id: 2487956
+      },
+      // {id: 2487889},
+      // {id: 2379574},
+      // {id: 2450022},
+      {
+        id: 2459115
+      }, {
+        id: 2514815
+      }, {
+        id: 2490383
+      }, {
+        id: 2436704
+      },
+      // {id: 2475687},
+      {
+        id: 2391279
+      }, {
+        id: 2486340
+      }, {
+        id: 2442047
+      }, {
+        id: 2471390
+      }
+    ]
+    // this for loop was to populate db with all locations listed above, making an api call to each location id
+  for (var i = 0; i < paramArray.length; i++) {
+    var params = paramArray[i];
 
-    for (var i = 0; i < apiArr[0].trends.length; i++) {
-      new LocationTrend({
-        location: apiArr[0].locations[0].name,
-        trend_name: apiArr[0].trends[i].name,
-        tweet_volume: apiArr[0].trends[i].tweet_volume
-      })
-      .save(function(err) {
-        if(err) throw err;
-        console.log('trend saved!');
-      });
-    }
+    client.get('trends/place', params, function(error, tweets, response) {
+      if (error) {
+        console.log('ERROR OCCURED', error);
+        throw error;
+      }
 
-    res.status(200);
-    res.send(tweets);
+      var apiArr = tweets;
 
-  });
+      for (var i = 0; i < apiArr[0].trends.length; i++) {
+        if (apiArr[0].trends[i].tweet_volume) {
+          new LocationTrend({
+              location: apiArr[0].locations[0].name,
+              trend_name: apiArr[0].trends[i].name,
+              tweet_volume: apiArr[0].trends[i].tweet_volume
+            })
+            .save(function(err) {
+              if (err) {
+                throw err;
+              }
+              console.log('trend saved to db!');
+            });
+        }
+      }
+      // res.status(200); // need to comment these out if you want to loop through several api calls to populate db
+      // res.send(tweets); // 
+
+    });
+
+  } // end for loop bracket
 });
 
 
