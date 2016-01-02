48b3678483193b8b910adbc92e7cbbdf2e7d22af
Simon Ding <simonding87@simons-mbp.attlocal.net>
2016-01-02 15:19:34 -0800

    [refactor] Refactor get city trends to twitterAPI folder

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ var LocationTrend = require('./mongodb/models/locationTrend.js');
 Local Dependencies
 **************************************************************/
 var countryTrends = require('./utilities/twitterAPI/countryTrends');
+var cityTrends = require('./utilities/twitterAPI/cityTrends');)
 var usRoutes = require('./utilities/middleware/usRoutes');
 
 /*************************************************************
@@ -73,101 +74,8 @@ app.get('/locationTrends', function(req, res) {
   });
 });
 
-// TESTING NEW API CALL
-
-var Twitter = require('twitter');
-
-var client = new Twitter({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token_key: process.env.ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET
-});
-
-app.get('/test', function(req, res) {
-
-  // specify id (WOEID) for each city then make a get request to the end point.
-  // These are stored on my local DB atm (Simon).
-  // Be careful not to call this same location twice or it will duplicate data in the DB.
-  // Maybe need to figure out how to prevent this.
-
-  // var params = {id: 2487956}; // San Francisco
-  // var params = {id: 2487889}; // San Diego
-  // var params = {id: 2379574}; // Chicago
-  // var params = {id: 2450022}; // Miami
-  // var params = {id: 2459115}; // New York (City)
-  // var params = {id: 2514815}; // Washington (D.C.)
-  // var params = {id: 2490383}; // Seattle
-  // var params = {id: 2436704}; // Las Vegas
-  // var params = {id: 2475687}; // Portland
-  // var params = {id: 2391279}; // Denver
-  // var params = {id: 2486340}; // Sacramento
-  // var params = {id: 2442047}; // Los Angeles
-  // var params = {id: 2471390}; // Phoenix
-  // var params = {id: }; // 
-
-  paramArray = [{
-        id: 2487956
-      }, {
-        id: 2487889
-      }, {
-        id: 2379574
-      }, {
-        id: 2450022
-      }, {
-        id: 2459115
-      }, {
-        id: 2514815
-      }, {
-        id: 2490383
-      }, {
-        id: 2436704
-      }, {
-        id: 2475687
-      }, {
-        id: 2391279
-      }, {
-        id: 2486340
-      }, {
-        id: 2442047
-      }, {
-        id: 2471390
-      }
-    ]
-    // this for loop was to populate db with all locations listed above, making an api call to each location id
-  for (var i = 0; i < paramArray.length; i++) {
-    var params = paramArray[i];
-
-    client.get('trends/place', params, function(error, tweets, response) {
-      if (error) {
-        console.log('ERROR OCCURED', error);
-        throw error;
-      }
-
-      var apiArr = tweets;
-
-      for (var i = 0; i < apiArr[0].trends.length; i++) {
-        if (apiArr[0].trends[i].tweet_volume) {
-          new LocationTrend({
-              location: apiArr[0].locations[0].name,
-              trend_name: apiArr[0].trends[i].name,
-              tweet_volume: apiArr[0].trends[i].tweet_volume,
-              created_at: apiArr[0].created_at
-            })
-            .save(function(err) {
-              if (err) {
-                throw err;
-              }
-              console.log('trend saved to db!');
-            });
-        }
-      }
-      // res.status(200); // need to comment these out if you want to loop through several api calls to populate db
-      // res.send(tweets); // 
-
-    });
-
-  } // end for loop bracket
+app.get('/populateDatabaseWithCityTrends', function(req, res) {
+  cityTrends.returnTrendsByCity();
 });
 
 
