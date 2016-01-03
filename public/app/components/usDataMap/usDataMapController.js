d1b0dfd32a1418081de1f4fb97d1aa7e3faef422
Polina Soshnin <psoshnin@wellesley.edu>
2016-01-03 14:54:04 -0800

    [feat] Add basic map of the united states

--- public/app/components/usDataMap/usDataMapController.js
+++ public/app/components/usDataMap/usDataMapController.js
@@ -1,5 +1,43 @@
-angular.module('usDataMap', [])
+var app = angular.module('usDataMap', ['datamaps']);
 
-.controller('usDataMapController', function($scope) {
+app.controller('usDataMapController', function($scope) {
   $scope.message = "us data map controller";
+
+  $scope.mapObject = {
+    scope: 'usa',
+    options: {
+      width: 1110,
+      legendHeight: 60 // optionally set the padding for the legend
+    },
+    geographyConfig: {
+      highlighBorderColor: '#EAA9A8',
+      highlighBorderWidth: 2
+    },
+    fills: {
+      'HIGH': '#CC4731',
+      'MEDIUM': '#306596',
+      'LOW': '#667FAF',
+      'defaultFill': '#DDDDDD'
+    },
+    data: {
+      "AZ": {
+        "fillKey": "MEDIUM",
+      },
+      "CO": {
+        "fillKey": "HIGH",
+      },
+      "DE": {
+        "fillKey": "LOW",
+      },
+      "GA": {
+        "fillKey": "MEDIUM",
+      }
+    },
+  };
+
+  
+
+
+
+
 });
