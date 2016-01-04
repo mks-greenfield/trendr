dfc6e2d22ae52c06eb114255677be1d43ee02818
Polina Soshnin <psoshnin@wellesley.edu>
2016-01-03 17:32:12 -0800

    [feat] add spinner

--- public/app/components/usDataMap/usDataMapController.js
+++ public/app/components/usDataMap/usDataMapController.js
@@ -13,9 +13,9 @@ app.controller('usDataMapController', function($scope,$http) {
       width: 800,
     },
     geographyConfig: {
-      highlighBorderColor: '#5bc0de',
+      highlighBorderColor: '#d9534f',
       highlighBorderWidth: 2,
-      highlightFillColor: '#5bc0de',
+      highlightFillColor: '#d9534f',
     },
     fills: {
       'BLUE': '#428bca',
@@ -25,6 +25,31 @@ app.controller('usDataMapController', function($scope,$http) {
     }
   };
 
+  var opts = {
+    lines: 13, // The number of lines to draw
+    length: 28, // The length of each line
+    width: 14, // The line thickness
+    radius: 42, // The radius of the inner circle
+    scale: 1.75, // Scales overall size of the spinner
+    corners: 1, // Corner roundness (0..1)
+    color: '#000', // #rgb or #rrggbb or array of colors
+    opacity: 0.25, // Opacity of the lines
+    rotate: 0, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    speed: 1, // Rounds per second
+    trail: 60, // Afterglow percentage
+    fps: 20, // Frames per second when using setTimeout() as a fallback for CSS
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    className: 'spinner', // The CSS class to assign to the spinner
+    top: '50%', // Top position relative to parent
+    left: '50%', // Left position relative to parent
+    shadow: false, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    position: 'absolute' // Element positioning
+  };
+  var target = document.getElementById('spinner');
+  var spinner = new Spinner(opts);
+
   $scope.selectState = function(geography) {
     $scope.$apply(function(){$scope.state = geography.properties.name;});
     $scope.getStatesDailyTrends(geography.properties.name);
@@ -32,13 +57,13 @@ app.controller('usDataMapController', function($scope,$http) {
 
   //get all states with available trends
   $scope.getStatesDailyTrends = function(stateName) {
-    // spinner.spin(target);
+    spinner.spin(target);
 
     $http({
       method: 'GET',
       url: '/api/us/states/'+stateName+'/dailyvolume'
     }).then(function successCallback(response) {
-        // spinner.stop();
+        spinner.stop();
         if (response.data[0] === "Empty") {
           $scope.trends = [];
           $scope.trend.error = "There are no available trends for this state. Please try another.";
